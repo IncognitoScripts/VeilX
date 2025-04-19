6b7af6bd78b4d50f920d18b114a3b9094fc2ac5e
IncognitoScripts <luka.jijavadzebrawl@gmail.com>
2025-04-19 21:47:32 +0400

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`VeilX website running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
